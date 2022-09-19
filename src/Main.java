0501c9ed2d4d717953ab7e5d4417d912a0602f2e
sametakgozz <samet-akgoz@outlook.com>
2022-09-19 21:25:53 +0300

    datatypes

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+      int sayi = 12;
+      sayi = -129;
+      char karakter = 'A';
+
+      boolean dogruMu = false;
+
+    }
+}
